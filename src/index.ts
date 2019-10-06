3cfd329f21c193c8c73ea64ec36406319e74e981
N1086706 <N1086706@ntub.edu.tw>
2019-10-06 14:03:13 +0800

    N1086706

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+let d:string='采'
+let e:string='玲'
+let f=d.concat(e)
+console.log(f)//連接兩個字串或更多字串，得到新字串
+let g:string=('mamie')
+console.log(g.toUpperCase())//把字串轉為大寫
+let h=123.456
+console.log(h.toFixed(5))//把數字轉為字串，並對小數點指定位數
+let i=9.87654
+console.log(i.toPrecision(2))//把數字格式化為指定長度
 // 請介紹兩個字串方法跟數字方法
 
+let a=40000
+let b=500000
 
-// 錢錢大於一個值，叫乾爹
-let money = 40000;
-let limit = 500000; // 限制值
+if(a<b){
+    console.log('乾爹')
+}else {
+    console.log('笑你')
+}
+
 
